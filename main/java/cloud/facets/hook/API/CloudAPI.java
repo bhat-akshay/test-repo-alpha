446e00ae438c08f3efc5b4f5d4c22bafea41c89f
akshaybhat <akshay@facets.cloud>
2022-07-08 19:16:22 +0530

    initial Commit

---
+++ main/java/cloud/facets/hook/API/CloudAPI.java
@@ -0,0 +1,21 @@
+package cloud.facets.hook.API;
+
+import com.cronutils.descriptor.CronDescriptor;
+import com.cronutils.model.CronType;
+import com.cronutils.model.definition.CronDefinition;
+import com.cronutils.model.definition.CronDefinitionBuilder;
+import com.cronutils.parser.CronParser;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("cloudAPI")
+public class CloudAPI {
+
+    @GetMapping("ep1/")
+    public String getData(){
+       return "HELLO";
+    }
+}
---
+++ main/java/cloud/facets/hook/HookApplication.java
@@ -0,0 +1,15 @@
+package cloud.facets.hook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+// App Name is Appplication
+@SpringBootApplication
+public class HookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HookApplication.class, args);
+
+    }
+}
